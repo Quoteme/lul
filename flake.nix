b2e7e73dc994a0cbfc8121367e8db50b715510d7
Luca Leon Happel <lucahappel99@gmx.de>
2023-01-16 14:03:03 +0100

    move from cabal to nix

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "A simple, quick and small window-manager";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        myGHCPackages = (hpkgs: with hpkgs; [
+          X11
+          ]);
+      in
+        rec {
+          defaultPackage = packages.lul;
+          packages.lul = pkgs.stdenv.mkDerivation {
+            name = "lul";
+            pname = "lul";
+            version = "1.0";
+            src = ./src;
+          
+            buildInputs = with pkgs; [
+              (haskellPackages.ghcWithPackages myGHCPackages)
+            ];
+            dontBuild = true;
+            # buildPhase = ''
+            #   ghc src/Main.hs -o lul
+            # '';
+            installPhase = ''
+              mkdir -p $out/bin
+              ghc src/Main.hs -o $out/bin/lul
+            '';
+          };
+        }
+      );
+}
